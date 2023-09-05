ed6f05ddffa4b9479f69fc3964ed0f2e67cfe9cc
Orhan Cansu <65315135+konerjonlar@users.noreply.github.com>
2023-09-05 16:05:33 +0300

    Add files via upload

---
+++ html-to-markdown.py
@@ -0,0 +1,17 @@
+
+from bs4 import BeautifulSoup
+import markdownify
+
+with open("html-code.txt", "r", encoding="utf-8") as dosya:
+    icerik = dosya.read()
+
+# HTML metnini parse et
+soup = BeautifulSoup(icerik, 'html.parser')
+
+# Markdown formatına dönüştür
+markdown_text = markdownify.markdownify(str(soup))
+
+# Dosyayı aç ve çıktıyı dosyaya yaz
+with open("html-to-markdown.txt", "w", encoding="utf-8") as dosya:
+    dosya.write(markdown_text)
+
